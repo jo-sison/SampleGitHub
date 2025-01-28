4e68fb76608a59568a32e0afcbcd19256f3b67e2
Cedric Dexter L. Tagle <cedricdextertagle@gmail.com>
2025-01-28 13:40:19 +0800

    checkbox true'

--- SampleGitHub/Form1.cs
+++ SampleGitHub/Form1.cs
@@ -20,6 +20,7 @@ namespace SampleGitHub
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello GitHUb!");
+            checkBox1.Checked = true;
         }
     }
 }
